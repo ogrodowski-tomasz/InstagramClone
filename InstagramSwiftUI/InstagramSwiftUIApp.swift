9cc135cb8bde7e0ac981a4874b9a318210f46e24
Tomasz Ogrodowski <iziman99@gmail.com>
2022-11-05 00:20:49 +0100

    Initial implementation of Firebase

--- InstagramSwiftUI/InstagramSwiftUIApp.swift
+++ InstagramSwiftUI/InstagramSwiftUIApp.swift
@@ -5,10 +5,16 @@
 //  Created by Tomasz Ogrodowski on 04/11/2022.
 //
 
+import Firebase
 import SwiftUI
 
 @main
 struct InstagramSwiftUIApp: App {
+    
+    init() {
+        FirebaseApp.configure()
+    }
+    
     var body: some Scene {
         WindowGroup {
 //            ContentView()
